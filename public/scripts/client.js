fe1e37ecb0fe9f8d9874fff04520a41415f846c9
Suleyman Omar <Suleymano780@live.com>
2021-12-18 02:18:33 +0000

    Fixing error messages

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -19,33 +19,13 @@ $(document).ready(function () {
     });
   };
   loadTweets();
+
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
   
-
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ];
-
 const createTweetElement = function(tweet) {
   let date = timeago.format(tweet.created_at);
   let $tweet = $(
@@ -111,5 +91,3 @@ const $newTweet = $('#tweet-form');
 })
 
 });
-
-
